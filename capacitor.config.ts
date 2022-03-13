80a8b8438873b5bc32acfa6b8b6adbd51ed5c31b
Mexson Fernandes <robomex2020@gmail.com>
2022-03-13 19:32:49 +0530

    :sparkles: add cap

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'nuxt-vuetify-cap',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
